e6afd78133bbf787b9feeb1ce4b7d51ca0f2492d
novix-vertex <167081171+novix-vertex@users.noreply.github.com>
2026-09-24 15:12:33 +0530

    - basic login api implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -18,7 +18,7 @@ export const registerController = async (req, res) => {
             });
         }
 
-        const isUserExisted = await userModel.findOne({email});
+        const isUserExisted = await userModel.findOne({ email });
 
         if (isUserExisted) {
             return res.status(409).json({
@@ -45,6 +45,48 @@ export const registerController = async (req, res) => {
 
     } catch (error) {
         console.error("Registration Error:", error);
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+export const loginController = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
+        const user = await userModel.findOne({ email }).select("+passwordHash");
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.passwordHash);
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        return res.status(200).json({
+            message: "User login successfully.",
+            data: {
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email
+                }
+            }
+        })
+    } catch (error) {
+        console.error("Login Error:", error);
         res.status(500).json({
             message: error.message
         })
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { registerController } from "../controllers/auth.controller.js";
+import { loginController, registerController } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", registerController);
+authRouter.post("/login", loginController);
 
 export default authRouter
