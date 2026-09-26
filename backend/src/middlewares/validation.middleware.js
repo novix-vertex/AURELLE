d7fdc6a159975f2e01c219c4db55256b0182a438
novix-vertex <167081171+novix-vertex@users.noreply.github.com>
2026-09-27 00:23:33 +0530

    - validate middleware response made shorter so that it only gives relevant field name and error message

--- backend/src/middlewares/validation.middleware.js
+++ backend/src/middlewares/validation.middleware.js
@@ -5,7 +5,10 @@ const validate = (req, res, next) => {
 
     if (!errors.isEmpty()) {
         return res.status(400).json({
-            errors: errors.array()
+            errors: errors.array().map((error) => ({
+                field: error.path,
+                message: error.msg
+            }))
         })
     }
     next();
