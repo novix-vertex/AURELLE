58b87170603ee1e813209d87ecf10afcdcac0e48
novix-vertex <167081171+novix-vertex@users.noreply.github.com>
2026-09-26 22:52:13 +0530

    added return in validate middle -  giving error as in case of validation errors

--- backend/src/middlewares/validation.middleware.js
+++ backend/src/middlewares/validation.middleware.js
@@ -4,7 +4,7 @@ const validate = (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        res.status(400).json({
+        return res.status(400).json({
             errors: errors.array()
         })
     }
